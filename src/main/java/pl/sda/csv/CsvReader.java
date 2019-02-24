bc150524780b42bc4df69c7c63dcc18c61e0c8f6
Michał Basiński <michal.basinski88@gmail.com>
2019-02-24 15:24:35 +0100

    csv reader stub

---
+++ src/main/java/pl/sda/csv/CsvReader.java
@@ -0,0 +1,14 @@
+package pl.sda.csv;
+
+import pl.sda.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvReader {
+
+    //TODO: zaimplementować metodę odczytującą pliki csv za danymi samochodów
+    public List<Car> read(String file) {
+        return new ArrayList<>();
+    }
+}
