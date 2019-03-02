aa05138babe961227424b2746072db0bacdd10b0
Michał Basiński <michal.basinski88@gmail.com>
2019-03-02 11:20:39 +0100

    Json reader step 2

--- src/main/java/pl/sda/json/JsonReader.java
+++ src/main/java/pl/sda/json/JsonReader.java
@@ -1,5 +1,7 @@
 package pl.sda.json;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import pl.sda.Car;
 
 import java.io.IOException;
@@ -9,18 +11,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonReader {
+
+    /**
+     * Metoda wczytująca plik JSON do reprezentacji javowej (lista obiektów Car)
+     * Schemat działania:
+     * 1. wczytanie zawartości pliku do tablicy bajtów z wykorzystaniem metody z klasy Files
+     * 2. skonwertowanie tablicy bajtów na stringa - wymagane jest podanie kodowania (standardowe i najbezpieczniejsze jest UTF-8)
+     * 3.
+     * @param filePath ścieżka do pliku
+     * @return
+     */
     public List<Car> read(String filePath) {
         List<Car> cars = new ArrayList<>();
 
-        // wczytanie zawartości pliku do tablicy bajtów z wykorzystaniem metody z klasy
-        // Files
-        byte[] rawFile = new byte[0];
         try {
-            rawFile = Files.readAllBytes(Paths.get(filePath));
-
-            // skonwertowanie tablicy bajtów na stringa - wymagane jest podanie kodowania
-            // (standardowe i najbezpieczniejsze jest UTF-8)
+            byte[] rawFile = Files.readAllBytes(Paths.get(filePath));
             String fileContents = new String(rawFile, "UTF-8");
+
+            JSONObject json = new JSONObject(fileContents);
+
+            JSONArray carsArray = json.getJSONArray("cars");
+
+
 
         } catch (IOException e) {
             throw new JsonReaderException(e.getMessage(), e);
